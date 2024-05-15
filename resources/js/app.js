5bacbebf606720ca59ea6671971056e22c38215c
Craig Harman <324026+craigharman@users.noreply.github.com>
2024-05-15 18:16:30 +0800

    Create an extension that stops page reload

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,18 @@ import 'animate.css'
 import Alpine from 'alpinejs'
 import htmx from 'htmx.org'
 
-window.hrmx = htmx
+// If a url request matches the current path then do not fetch it
+htmx.defineExtension('no-load', {
+  onEvent: function (name, evt) {
+    if (name === 'htmx:confirm') {
+      if (evt.detail.path === window.location.pathname) {
+        evt.preventDefault() // Cancel the event
+      }
+    }
+  },
+})
+
+window.htmx = htmx
 htmx.config.defaultSwapStyle = 'outerHTML'
 
 window.Alpine = Alpine
